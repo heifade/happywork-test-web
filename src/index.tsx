b06669a5e9101718d228c5c8902a3ba2e9446eae
heifade <heifade@126.com>
2018-06-25 17:42:47 +0800

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppComponent } from "./app/app";
 
-let theme = require("./theme/theme.less");
+const styles = require("./theme/theme.less");
+
 
 let div = document.createElement("div");
 document.body.appendChild(div);
-div.className = theme["app-theme"];
+div.className = styles["app-theme"];
 
 ReactDOM.render(<AppComponent />, div);
